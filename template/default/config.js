1e9a2e321772fcd4353fb1acaf0a51d83b80f525
cczw2010 <71752352@qq.com>
2015-05-11 18:46:51 +0800

    添加image支持，但是不压缩

--- template/default/config.js
+++ template/default/config.js
@@ -4,7 +4,7 @@
  * @ename 应用英文名称
  * @descriptyion  应用描述
  * @version  应用版本,每次release生成的版本名称也是依据此处，请使用三段版本
- * @watch 工程需要处理的目录，包括js|css|html	,image本版本不再处理
+ * @watch 工程需要处理的目录，包括js|css|image|html	,image本版本不做压缩处理
  * 
  * @main  入口html页面,测试需要知道入口。
  * @port	工程测试服务端口
@@ -17,13 +17,14 @@ module.exports = {
 		ename:'scv-demo',
 		description:'scv工程流模板工程',
 		version:'1.0.0',
-		watch:['js','css','html'],
+		watch:['js','css','image','html'],
 		// 测试参数
 		// main:'html/index.html',
 		// port:8001,
 		//发布参数
 		assetsDomain:{
 				'js':'',	// http://cdn.xxx.com/js/
-				'css':''	//http://css.xxx.com/xx/
+				'css':'',	//http://css.xxx.com/css/
+				'image':''	//http://css.xxx.com/i/
 			},
 	};
